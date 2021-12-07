a452e03d4fdde05f63c243b33d101fe3c6594c88
Radoslav Genchev <rgenchev@hey.com>
2021-12-07 23:22:39 +0200

    Day 2: initialize entries before the submarine

--- 2021/day_02/dive.rb
+++ 2021/day_02/dive.rb
@@ -2,8 +2,8 @@ require_relative 'submarine'
 
 class Dive
   def self.call
+    entries = File.readlines('input.txt').map { |measurement| measurement.strip.split }    
     submarine = Submarine.new
-    entries = File.readlines('input.txt').map { |measurement| measurement.strip.split }
 
     entries.each do |direction, units|
       submarine.move(direction, units)
